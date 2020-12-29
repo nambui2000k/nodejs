5833384338f8e08ad784f5227d756e51dc96e7a5
Bui Duc Nam <doubledev@gmail.com>
2020-12-29 13:48:48 +0700

    asf

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let http = require('http').createServer(app)
 const port = process.env.PORT || 2000
 
 app.get("/",function (request,response) {
-        response.send("Hello")
+        response.send("Hello 123")
 })
 
 http.listen(port,()=>{
